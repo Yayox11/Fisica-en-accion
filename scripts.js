aff0b94de599952b441923d019a41bd5d810a56c
LAPTOP-2VCBBLJO\gabriel <gabriel.astorga@sansano.usm.cl>
2018-10-20 01:46:56 -0300

    Gabo was here 5

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 
 $(document).ready(function(){
   var scroll_start = 0;
-  var startchange = $('#features12-6');
+  var startchange = $('#index');
   var offset = startchange.offset();
-  var startchange2 = $('#sect2');
+  var startchange2 = $('#servicios2');
   var offset2 = startchange2.offset();
+  var startchange3 = $('#productos2');
+  var offset3 = startchange3.offset();
+  var startchange4 = $('#quienes_somos2');
+  var offset4 = startchange4.offset();
 
   if(startchange.length){
     $(document).scroll(function(){
       scroll_start = $(this).scrollTop();
-        console.log(offset.top + scroll_start);
-        console.log(offset.top);
+        //console.log(offset.top + scroll_start);
+        //console.log(offset.top);
       if(offset.top == (offset.top + scroll_start)){
         $('.navbar-custom').css('background-color', '#0e0e0e1f');
       }else{
@@ -21,11 +25,34 @@ $(document).ready(function(){
   if(startchange2.length){
     $(document).scroll(function(){
       scroll_start = $(this).scrollTop();
-
-      if(offset.top == (offset2.top + scroll_start)){
-        $('.navbar-custom').css('background-color', 'rgba(255, 255, 255, 0.83)');
+      if(offset2.top == (offset2.top + scroll_start)){
+        $('.navbar-custom').css('background-color', 'rgba(255, 255, 255, 0)');
       }else{
-        $('.navbar-custom').css('background-color', '#fff');
+        $('.navbar-custom').css('background-color', 'rgb(15, 118, 153)');
+      }
+    })
+  }
+  
+  if(startchange3.length){
+    $(document).scroll(function(){
+      scroll_start = $(this).scrollTop();
+
+      if(offset3.top == (offset3.top + scroll_start)){
+        $('.navbar-custom').css('background-color', 'rgba(255, 255, 255, 0)');
+      }else{
+        $('.navbar-custom').css('background-color', '#00a8ff');
+      }
+    })
+  }
+  
+  if(startchange4.length){
+    $(document).scroll(function(){
+      scroll_start = $(this).scrollTop();
+
+      if(offset4.top == (offset4.top + scroll_start)){
+        $('.navbar-custom').css('background-color', 'rgba(255, 255, 255, 0)');
+      }else{
+        $('.navbar-custom').css('background-color', '#B53471');
       }
     })
   }
